0dfce65e4258d83cea366e3b4b5ef6d7547b6d48
Weston Renoud <wrenoud@gmail.com>
2018-12-04 11:13:41 +0100

    initial day 4 code

--- day_04.py
+++ day_04.py
@@ -8,8 +8,19 @@ def ReadData():
 		d = datetime.strptime(line[1:17],"%Y-%m-%d %H:%M")
 		m = line[19:].strip()
 		data.append((d,m))
-	return data
+	return sorted(data, key=lambda v: v[0])
+
+
+def part1(inputs):
+	sleeps = {}
+	guard = None
+	for t, message in inputs:
+		if "Guard" in message:
+			_, guard, _, _ = message.split(' ')
+			if guard not in sleeps:
+				sleeps[guard] = {}
 
 if __name__ == "__main__":
 	inputs = ReadData()
-	print(inputs)
+
+	
