76092a01bfc06e76cefb2b1a19192fcc507d319f
Weston Renoud <wrenoud@gmail.com>
2018-12-05 15:57:45 -0500

    fix answer to part 2

--- day_05.py
+++ day_05.py
@@ -1,42 +1,54 @@
 import util
+import string
 
-
-def dist(a,b):
-	return abs(ord(a)-ord(b))
+def same(a, b):
+	return a.lower() == b.lower()
+	
+def will_react(a,b):
+	return a != b and same(a,b)
 
 
 def react(letters):
 	i = len(letters)
-	tmp = letters[:]
+	tmp = letters[:] # create a local copy
 	while i > 1:
-		i-=1
-		if dist(tmp[i], tmp[i-1]) == 32:
+		i -= 1
+		if will_react(tmp[i], tmp[i-1]):
 			del tmp[i]
 			del tmp[i-1]
-			if i > len(tmp): i = len(tmp)
+			if i > len(tmp): i -= 1
 	return tmp
 
 
 def part1(letters):
-	letters = react(letters)
-	util.Answer(1, len(letters))
+	util.Answer(1, len(react(letters)))
 
 
 def part2(letters):
+	letters = react(letters) # start from the reacted string
+	
 	letter = None
 	length = None
-	for v in range(ord('a'),ord('z')+1):
-		t = chr(v)
-		tmp = react(list(l for l in letters if l.lower() != t))
-		if letter is None or len(tmp) < length:
-			letter = t
-			length = len(tmp)
-		print(t,len(tmp))
+	for c in string.ascii_lowercase:
+		tmp = list(l for l in letters if not same(l, c))
+		count = len(react(tmp))
+		if letter is None or count < length:
+			letter = c
+			length = count
 	util.Answer(2, (letter, length))
 
 
 if __name__ == "__main__":
+	assert will_react('a', 'A')
+	assert will_react('A', 'a')
+	assert not will_react('a', 'a')
+
+	assert same('a', 'A')
+	assert same('A', 'a')
+	assert same('a', 'a')
+	
 	data, = util.ReadData(5)
-	letters = list(c for c in data)
+	letters = list(c for c in data) # bust the string out into a list
+	
 	part1(letters)
 	part2(letters)
