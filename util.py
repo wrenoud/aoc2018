cb2a9b6fd61f1bd803f5e6a07747e170d583310f
Weston Renoud <wrenoud@gmail.com>
2018-12-05 16:28:32 -0500

    add puzzle download functionality

--- util.py
+++ util.py
@@ -1,14 +1,29 @@
+import os
+import json
 from typing import List
+import requests
+
+
+def get_session():
+	env = json.load(open('env.json'))	
+	cookie = requests.cookies.create_cookie(name='session', value=env['session'])
+	sess = requests.session()
+	sess.cookies.set_cookie(cookie)
+	return sess
 
 
 def ReadData(day:int) -> List[str]:
-	with open("./data/day_{:02}/puzzle.txt".format(day)) as f:
-		return f.readlines()
-
-
-def ReadTestData(day:int) -> List[str]:
-	with open("./data/day_{:02}/test.txt".format(day)) as f:
-		return f.readlines()
+	filepath = "./data/puzzle_{:02}.txt".format(day)
+	
+	if not os.path.exists(filepath):
+		url = "https://adventofcode.com/2018/day/{}/input".format(day)
+		puzzle = get_session().get(url).text.strip()
+		with open(filepath, 'w') as f:
+			f.write(puzzle)
+		return puzzle.split('\n')
+		
+	with open(filepath) as f:
+		return f.read().split('\n')
 
 
 def Answer(part, answer):
@@ -20,4 +35,5 @@ class AdventOfCodeDay:
 
 
 if __name__ == "__main__":
-	print(ReadData(3))
+	
+	print(ReadData(1))
