04880406d0718ac8091e8f1c1d4bf609d79dd808
Weston Renoud <wrenoud@gmail.com>
2018-12-12 21:36:42 +0100

    thoughts on day 12

--- day_12.py
+++ day_12.py
@@ -1,15 +1,29 @@
 import util
 
 
-def part1(data):
+def part1(start, transitions):
+	current = list(start[:])
+	next = list(start[:])
+	for gen in range(20):
+		for i in range(len(start)-5):
+			for match, state in transitions:
+				if current[i:i+5] == match:
+					next[i+2]=state
+		current = next
+		print(''.join(current))
 	util.Answer(1, None)
 
 		
-def part2(data):
+def part2(start, transitions):
 	util.Answer(2, None)
 
 
 if __name__ == "__main__":
 	data = util.ReadPuzzle()
-	part1(data)
-	part2(data)
+	start = data[0][15:]
+	transitions = []
+	for line in data[2:]:
+		match,state = line.split(' => ')
+		transitions.append((list(match[:]),state))
+	part1(start, transitions)
+	part2(start, transitions)
